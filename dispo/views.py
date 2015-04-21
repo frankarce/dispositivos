3da98f41286903048b2957da1fd78f206e86aca2
frank <francisco.j.arce@gmail.com>
2015-04-21 10:10:47 -0700

    validacion de tipo de archivo

--- dispo/views.py
+++ dispo/views.py
@@ -1,22 +1,27 @@
-# Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, RequestContext
 import random,json, redis
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_exempt
-
+ipmaster="127.0.0.1"
 def device(request,id):
 
     inform=redis.Redis("localhost")
     a=inform.get(id)
-    b=a.split(",")
-    data=({"url":b[0],"reproduce":b[1],"tipo":b[2]})
 
-    if(b[1]=="restart"):
-        inform.set(id,+b[0]+",play")
+    b=eval(a)
 
-    dataj=json.dumps(data)
+    #data1=json.loads(b)
+    #print data1,type(data1)
 
-    return HttpResponse(dataj,mimetype='application/json')
+    ################  Este if es para el caso de que se necesite reiniciar un video###########################################
+    #if(a[1]=="restart"):
+    #    inform.set(id,+b[0]+",play")
+    #print data1
+    dataj=json.dumps(b)
+    #d=json.loads(dataj)
+    #print type(d)
+
+    return HttpResponse(dataj)
 
 def index(request,id):
     return render_to_response("index.html",{'id':id})
